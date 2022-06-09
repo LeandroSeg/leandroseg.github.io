30765007e13887752d574d9915bc7f6c2f36a6d2
LeandroSeg <dragonavenger@gmail.com>
2022-06-09 15:31:11 -0300

    Add files via upload

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -21,9 +21,9 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "0ca311bb5b6799ddb422e6bcef49eba5",
-"/": "0ca311bb5b6799ddb422e6bcef49eba5",
-"main.dart.js": "9ff0288edc8f8158cca794d598e3dc36",
+"index.html": "1e030405e9119e9dce515e6540ac7ce4",
+"/": "1e030405e9119e9dce515e6540ac7ce4",
+"main.dart.js": "8f34faa5543b192efda73695a25a2db9",
 "manifest.json": "74b2fe66860d3d5b9d2301f3cbc0df16",
 "version.json": "2c856dd849ee688f08d0b503330bfc58"
 };
